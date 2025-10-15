46d0718e713e91a2d42375ce5a24ff69de8bc255
Vinicius  Santiago <149703728+Viniciustsant@users.noreply.github.com>
2025-10-15 12:10:34 -0300

    Adding the get itens

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -4,9 +4,13 @@ from pydantic import BaseModel, EmailStr
 
 app = FastAPI() 
 
-items = [] # Variavel global, banco de dados em memoria (lista para itens)
+itens = [] # Variavel global, banco de dados em memoria (lista para itens)
 
 class Item(BaseModel):
     id: int # definição do tipo de dado para numero inteiro, será incremental
     name: str # definição do tipo de dado para string
     email: EmailStr # definição do tipo de dado para email valido, validado pelo Pydantic
+
+@app.get("/itens") #endpoint com Get(/itens)
+def get_itens():
+    return itens #returning the itens list
