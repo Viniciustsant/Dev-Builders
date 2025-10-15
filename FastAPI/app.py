7277184defe12fa11309e91107e72c259c63f773
Vinicius  Santiago <149703728+Viniciustsant@users.noreply.github.com>
2025-10-15 12:26:06 -0300

    Adding the get itens/id

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -4,13 +4,20 @@ from pydantic import BaseModel, EmailStr
 
 app = FastAPI() 
 
-itens = [] # Variavel global, banco de dados em memoria (lista para itens)
+itens = [] # array in memory
 
 class Item(BaseModel):
     id: int # definição do tipo de dado para numero inteiro, será incremental
     name: str # definição do tipo de dado para string
     email: EmailStr # definição do tipo de dado para email valido, validado pelo Pydantic
 
-@app.get("/itens") #endpoint com Get(/itens)
+@app.get("/itens") # endpoint com get/itens
 def get_itens():
-    return itens #returning the itens list
+    return itens # returning the itens list
+
+@app.get("/itens/{item_id}") # endpoint with get/itens/id to find a specifc ID
+def get_id(item_id: int): # using the ID with parameter
+    for item in itens: # loop 
+        if item.id == item_id:
+            return item # 200 OK automatic
+    raise HTTPException(status_code=404, detail="Item not found") # error message
