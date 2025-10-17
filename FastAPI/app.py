ab7e68c73aea71483cfde91904bf0b4624e9c87f
Vinicius  Santiago <149703728+Viniciustsant@users.noreply.github.com>
2025-10-17 12:50:50 -0300

    adding the put endpoint

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -23,9 +23,18 @@ def get_id(item_id: int): # using the ID with parameter
     raise HTTPException(status_code=404, detail="Item not found") # error message
 
 @app.post("/itens", status_code=status.HTTP_201_CREATED) # endpoint post to add a new item, and the message to show if it's completed
-def create_new_item(item: Item): #using item to fastApi to find the itens name and email automatically
+def create_new_item(item: Item): #using item for fastApi to find the itens name and email automatically
     new_id = len(itens) + 1 # creating the new id using the lenght of the list and adding 1 more
     item.id = new_id # the id from the list will be this new ID
     itens.append(item) #adding the item to the list
     return item
     
+@app.put("/itens/{item_id}") # endpoint put to update an item inside the list
+def update_item(item_id: int, item: Item): # 
+    for existing_item in itens: # array on the list, using existing item 
+        if existing_item.id == item_id: # finding the correct id
+            existing_item.name = item.name  # changing the name 
+            existing_item.email = item.email #changing the email 
+            return existing_item # returning the updated item to the list
+    raise HTTPException(status_code=404, detail="Item not found") # error message
+        
